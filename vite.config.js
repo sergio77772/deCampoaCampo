c1e73c91c61ad73649854ae42953b307612a3a2c
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-09-15 20:29:59 -0300

    init: scaffold vite + react project

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@features': resolve(__dirname, './src/features'),
+      '@services': resolve(__dirname, './src/services'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@styles': resolve(__dirname, './src/styles'),
+      '@app': resolve(__dirname, './src/app'),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    // Chunking para mejor performance
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
+          router: ['react-router-dom'],
+          charts: ['recharts'],
+          forms: ['formik', 'yup'],
+          styling: ['styled-components'],
+        },
+      },
+    },
+  },
+});
